80ff1267b6c0fd01d70735638d460ec2fef9e0f2
DmytroMuzhenko <dimakpimuzhenko@gmail.com>
2024-12-21 20:18:24 +0200

    feat(Data): func to gen fake sensor data, with tests

---
+++ src/tests/generateMockData.test.js
@@ -0,0 +1,36 @@
+const { generateEnergyLogs } = require('../mockData/dataGen.js');
+
+require('dotenv').config({ silent: true });
+
+describe('Generate mock data', () => {
+  test('Generate mock sensor data for energy consumptionin 1 hour', () => {
+    expect.assertions(1);
+
+    const deviceName = 'test_device';
+    const logFileName = `energy_logs_${deviceName}.json`;
+
+    generateEnergyLogs({
+      totalTime: 60,
+      intervalTime: 1,
+      deviceName,
+    });
+
+    const logs = require(`../../logs/${logFileName}`);
+
+    expect(logs).toStrictEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          timestamp: expect.any(String),
+          device: deviceName,
+          voltage_bus: expect.any(Number),
+          current: expect.any(String),
+          power: expect.any(String),
+          load_voltage: expect.any(Number),
+          shunt_voltage: expect.any(String),
+          calibration: expect.any(Number),
+          energy_consumed_wh: expect.any(String),
+        }),
+      ]),
+    );
+  });
+});
--- src/tests/hello.test.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config({ silent: true });
-
-describe('initial', () => {
-  test('hello', () => {
-    console.log('hello');
-  });
-});
